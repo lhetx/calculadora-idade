f59e50fd511b19b615da530bfa3596a7795b0552
Henrique <henrique.takahashia@gmail.com>
2026-09-09 11:51:09 -0300

    feat: implementa cálculo da idade aproximada

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,15 @@
+from datetime import date
+
 def obter_ano_nascimento():
     return int(input("Digite seu ano de nascimento: "))
 
+def calcular_idade(ano_nascimento):
+    return date.today().year - ano_nascimento
+
 def main():
     ano = obter_ano_nascimento()
-    print(f"Ano informado: {ano}")
+    idade = calcular_idade(ano)
+    print(f"Sua idade aproximada é {idade} anos.")
 
 if __name__ == "__main__":
     main()
